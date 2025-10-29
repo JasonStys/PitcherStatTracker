725d1b4507e2211511f80f6a43760aaf8075e023
Enlight1 <Enlight1@users.noreply.github.com>
2025-10-29 15:00:53 -0700

    Create Pitch object

--- src/edu/csusm/cs370/team8/pitcherstattracker/model/Session.java
+++ src/edu/csusm/cs370/team8/pitcherstattracker/model/Session.java
@@ -1,8 +1,11 @@
 package edu.csusm.cs370.team8.pitcherstattracker.model;
 
+import java.util.ArrayList;
+
 /* This represents a single "Game" or "Period of training", usually 1-2 hours.
  * THIS CLASS IS WHAT HAS ALL THE STAT FIELDS.
  */
 public class Session {
     // Needs a Timestamp but I need to look at what's the best java module to use for that.
+    ArrayList<Pitch> pitch; // All the different games and trainings this pitcher has done.
 }
