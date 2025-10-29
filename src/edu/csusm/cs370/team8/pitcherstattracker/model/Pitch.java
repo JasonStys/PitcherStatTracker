eacf85096a62819afddece472049421f5d232a27
Enlight1 <Enlight1@users.noreply.github.com>
2025-10-29 16:06:52 -0700

    Update Pitch.java
    Basic stats for now

--- src/edu/csusm/cs370/team8/pitcherstattracker/model/Pitch.java
+++ src/edu/csusm/cs370/team8/pitcherstattracker/model/Pitch.java
@@ -1,4 +1,22 @@
 package edu.csusm.cs370.team8.pitcherstattracker.model;
 
 public class Pitch {
+    public enum type {
+        Curveball, Fastball, Knuckleball
+    }
+    public enum Result {
+        CalledStrike(0), Ball(1), Foul(2), Hit(3);
+        private final int value;
+        Result(int input) { this.value = input; }
+        public int toInt() { return value; }
+    }
+    private Result result;
+
+    private int bases;
+    private boolean inZone;
+    private int  id;
+    private double speed;
+    private boolean wasSwungAt;
+
+
 }
