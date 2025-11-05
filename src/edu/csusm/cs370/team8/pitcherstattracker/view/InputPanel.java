1e051f8d311d8804ba74aa75c5e92d2c0ae79f29
Enlight1 <Enlight1@users.noreply.github.com>
2025-11-05 15:41:22 -0800

    Unfinished Input Form
    Run MainWindow for Input Form

--- src/edu/csusm/cs370/team8/pitcherstattracker/view/InputPanel.java
+++ src/edu/csusm/cs370/team8/pitcherstattracker/view/InputPanel.java
@@ -1,4 +1,27 @@
 package edu.csusm.cs370.team8.pitcherstattracker.view;
 
-public class InputPanel {
+import edu.csusm.cs370.team8.pitcherstattracker.model.*;
+
+import javax.swing.*;
+
+public class InputPanel extends javax.swing.JPanel {
+    public InputPanel() {
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        initComponents();
+        this.setVisible(true);
+    }
+    //@SuppressWarnings("unchecked")
+    private void initComponents() {
+        JComboBox typeList = new JComboBox(Pitch.Type.values());
+        typeList.setSelectedIndex(1);
+        this.add(typeList);
+        //typeList.getSelectedIndex();
+
+        JComboBox resultList = new JComboBox(Pitch.Result.values());
+        resultList.setSelectedIndex(1);
+        this.add(resultList);
+        //typeList.getSelectedIndex();
+
+        
+    }
 }
